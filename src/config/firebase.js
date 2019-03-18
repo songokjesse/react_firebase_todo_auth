1127d97e846a11dbad14b16082f811fcd2c15f5a
Jesse Songok <jessesongok@gmail.com>
2019-03-18 12:14:30 +0300

    Added firebase config file

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+    databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
+    projectId: process.env.REACT_APP_FIREBASE_projectId,
+    storageBucket:process.env.REACT_APP_FIREBASE_storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId
+};
+firebase.initializeApp(config);
+export  default firebase;
